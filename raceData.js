b38fbb0a299e430b5ab0585ce4178bcc699ed7b9
Mitchell Deane <mitchelld20@yahoo.com>
2017-02-13 00:32:00 -0600

    Completed all the basic data for Core Races, Featured Races, and Uncommon races. Will add more later.

---
+++ raceData.js
@@ -0,0 +1,562 @@
+module.exports = {
+	racialModifierList: {
+	
+		human: {
+			name: "Human",
+			modifiers: {
+				plusTwoToAny: true,
+				strength: 0,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: true,
+			coreRace: true,
+			thirdParty: false
+		},			
+		elf: {
+			name: "Elf",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: -2,
+				intelligence: 2,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: true,
+			coreRace: true,
+			thirdParty: false
+		},
+		gnome: {
+			name: "Gnome",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 0,
+				constitution: 2,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: true,
+			coreRace: true,
+			thirdParty: false
+		},
+		halfElf: {
+			name: "Half-Elf",
+			modifiers: {
+				plusTwoToAny: true,
+				strength: 0,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: true,
+			coreRace: true,
+			thirdParty: false
+		},
+		halfling: {
+			name: "Halfling",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: true,
+			coreRace: true,
+			thirdParty: false
+		},
+		halfOrc: {
+			name: "Half-Orc",
+			modifiers: {
+				plusTwoToAny: true,
+				strength: 0,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: true,
+			coreRace: true,
+			thirdParty: false
+		},
+		dwarf: {
+			name: "Dwarf",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 0,
+				constitution: 2,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: -2
+			},
+			pfsLegal: true,
+			coreRace: true,
+			thirdParty: false
+		},
+		aasimar: {
+			name: "Aasimar",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		catfolk: {
+			name: "Catfolk",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: -2,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		dhampir: {
+			name: "Dhampir",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: -2,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		drow: {
+			name: "Drow",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: -2,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		fetchling: {
+			name: "Fetchling",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: -2,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		goblin: {
+			name: "Goblin",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 4,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: -2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		hobgoblin: {
+			name: "Hobgoblin",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 2,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		ifrit: {
+			name: "Ifrit",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: -2,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		kobold: {
+			name: "Kobold",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -4,
+				dexterity: 2,
+				constitution: -2,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		orc: {
+			name: "Orc",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 4,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: -2,
+				wisdom: -2,
+				charisma: -2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		oread: {
+			name: "Oread",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 2,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: -2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		ratfolk: {
+			name: "Ratfolk",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 2,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		sylph: {
+			name: "Sylph",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: -2,
+				intelligence: 2,
+				wisdom: 0,
+				charisma: 0
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		tengu: {
+			name: "Tengu",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: -2,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: 0
+			},
+			pfsLegal: true,
+			coreRace: false,
+			thirdParty: false
+		},
+		tiefling: {
+			name: "Tiefling",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 2,
+				wisdom: 0,
+				charisma: -2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		undine: {
+			name: "Undine",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: 0
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		changling: {
+			name: "Changeling",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 0,
+				constitution: -2,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		duergar: {
+			name: "Duergar",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 0,
+				constitution: 2,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: -4
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		gillmen: {
+			name: "Gillmen",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 0,
+				constitution: 2,
+				intelligence: 0,
+				wisdom: -2,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		grippli: {
+			name: "Grippli",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: 0
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		kitsune: {
+			name: "Kitsune",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: true,
+			coreRace: false,
+			thirdParty: false
+		},
+		merfolk: {
+			name: "Merfolk",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 2,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		nagaji: {
+			name: "Nagaji",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 2,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: -2,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: true,
+			coreRace: false,
+			thirdParty: false
+		},
+		samsaran: {
+			name: "Samsaran",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 0,
+				constitution: -2,
+				intelligence: 2,
+				wisdom: 2,
+				charisma: 0
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		strix: {
+			name: "Strix",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 0,
+				charisma: -2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		suli: {
+			name: "Suli",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 2,
+				dexterity: 0,
+				constitution: 0,
+				intelligence: -2,
+				wisdom: 0,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		svirfneblin: {
+			name: "Svirfneblin",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: -2,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: -4
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		vanara: {
+			name: "Vanara",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: 2,
+				charisma: -2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		vishkanya: {
+			name: "Vishkanya",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 0,
+				wisdom: -2,
+				charisma: 2
+			},
+			pfsLegal: false,
+			coreRace: false,
+			thirdParty: false
+		},
+		wayang: {
+			name: "Wayang",
+			modifiers: {
+				plusTwoToAny: false,
+				strength: 0,
+				dexterity: 2,
+				constitution: 0,
+				intelligence: 2,
+				wisdom: -2,
+				charisma: 0
+			},
+			pfsLegal: true,
+			coreRace: false,
+			thirdParty: false
+		},
+	}
+
+
+}
